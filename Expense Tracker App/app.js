a5acc6275f1944c70f932fe255d173c398b5e0db
Ajay  Morade <103033106+AjayMorde@users.noreply.github.com>
2025-12-17 16:35:52 +0530

    Add files via upload

---
+++ Expense Tracker App/app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const cors = require('cors');
+
+
+
+
+const sequelize = require('./connections/database');
+
+const Expense = require('./models/expense');
+const Users = require('./models/user');
+const Order = require('./models/order');
+const Forgotpassword = require('./models/forgotPassword');
+const Download = require('./models/download');
+
+
+const mainPageRouter = require('./routes/mainpage');
+const addUser = require('./routes/add-user');
+const userName = require('./routes/getUsername');
+const userLogin = require('./routes/user-login');
+const addExpense = require('./routes/add-expense');
+const getExpense = require('./routes/get-expense');
+const deleteExpense = require('./routes/delete-expense');
+
+
+const forgotPassword = require('./routes/forgotPassword');
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.use(mainPageRouter)
+app.use('/add-user', addUser);
+app.use('/user-login', userLogin);
+app.use('/getUser', userName);
+app.use('/add-expense', addExpense);
+app.use('/get-expense', getExpense);
+app.use('/delete-expense', deleteExpense);
+// app.use('/purchase', purchase);
+// app.use('/premiumuser', premiumFeatures);
+app.use('/password', forgotPassword);
+// app.use('/report', report);
+
+
+Users.hasMany(Expense);
+Expense.belongsTo(Users);
+
+
+Users.hasMany(Order);
+Order.belongsTo(Users);
+
+
+Users.hasMany(Forgotpassword);
+Forgotpassword.belongsTo(Users);
+
+Users.hasMany(Download);
+Download.belongsTo(Users);
+
+
+sequelize.authenticate()
+  .then(() => {
+    console.log("✅ Database connected");
+    return sequelize.sync();
+  })
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(" Server started on port 3000");
+    });
+  })
+  .catch(err => {
+    console.error("❌ DB error:", err);
+  });
